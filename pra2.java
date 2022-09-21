0d139867ec9c1aec2fc74a5a660767f64aeb5d82
sakamoto <s.sakamoto.sys22@morijyobi.ac.jp>
2022-09-21 12:39:02 +0900

    Hello Worldを１００回出力

--- pra2.java
+++ pra2.java
@@ -1,5 +1,8 @@
 class Practice2{
 	public static void main(String[] args){
 		System.out.println("Hello World");
+		for( String i = 0; i <= 100; i++){
+		System.out.println("Hello World");
+	}
 	}
 }
